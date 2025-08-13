1f1a81a9707bb6ca85e8f5336868747e2b0f46b0
Arthur Wagenknecht <oarthurdev@gmail.com>
2025-08-13 12:33:34 -0300

    changes

--- client/src/pages/BrokerProfilePage.tsx
+++ client/src/pages/BrokerProfilePage.tsx
@@ -561,9 +561,9 @@ export function BrokerProfilePage() {
                     </div>
                     <div>
                       <h3 className="text-sm font-medium text-gray-400">
-                        Angariações
+                        Leads
                       </h3>
-                      <p className="text-sm text-gray-500">leads captados</p>
+                      <p className="text-sm text-gray-500">recebidos</p>
                     </div>
                   </div>
                   <p className="text-2xl font-bold text-white">
